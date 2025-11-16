9853b2bd996be7bcbdd8f1589fba72b020d88d2a
helloworldxdwastaken <hello@noxamusic.com>
2025-11-16 14:45:15 +0200

    Fix RN host crash and bundle vector icons

--- android/app/src/main/java/com/noxamusic/app/MainApplication.kt
+++ android/app/src/main/java/com/noxamusic/app/MainApplication.kt
@@ -4,19 +4,34 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.noxamusic.app.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
+  private val defaultReactNativeHost: DefaultReactNativeHost by lazy {
+    object : DefaultReactNativeHost(this) {
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+      override fun getPackages() =
+        PackageList(this@MainApplication).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        }
+
+      override fun getJSMainModuleName(): String = "index"
+    }
+  }
+
+  override val reactNativeHost: ReactNativeHost
+    get() = defaultReactNativeHost
 
   override val reactHost: ReactHost by lazy {
     getDefaultReactHost(
       context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
+      reactNativeHost = defaultReactNativeHost,
     )
   }
 
